dd61865ad65c023227f329b676b4f46c86bdf6c8
Student <student@Students-MacBook-Pro.local>
2022-07-02 14:04:31 +0100

    Not sure

--- src/components/SeasonSelect.js
+++ src/components/SeasonSelect.js
@@ -2,23 +2,22 @@ import React from "react";
 
 function SeasonSelect ({episodes}) {
   const uniqueSeasonNumbers = [];
-  const seasonOptions = episodes.filter(episode => {
-    <option></option>
+  var seasonOptions = episodes.filter(episode => {
     const isDuplicate = uniqueSeasonNumbers.includes(episode.season);
-    if (!isDuplicate) {
-      uniqueSeasonNumbers.push(episode.season);
-
-      // return true;
-    }
-    // return false;
-    <option>{uniqueSeasonNumbers}</option>
-  });
-  console.log(uniqueSeasonNumbers)
+      if (!isDuplicate) {
+        uniqueSeasonNumbers.push(episode.season);
+      }
+    });
   
+  console.log(uniqueSeasonNumbers)
+
   return (
   <>
   Please select a season:  
+  {/* <select>{uniqueSeasonNumbers}</select> */}
+  {uniqueSeasonNumbers}
   <select>{seasonOptions}</select>
+  <hr></hr>
   </>
   )
 }
